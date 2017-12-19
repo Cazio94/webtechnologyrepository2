b6fd8fb2e0c6b95f01ef052d05b9d5eedc60dd42
cazio94 <a.catani22@gmail.com>
2017-12-19 16:45:17 +0100

    CSS added

--- ucicinemas/views.py
+++ ucicinemas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, 'ucicinemas/index.html', {})
+    movies = Movie.objects.all().order_by('title')
+    return render(request, 'ucicinemas/index.html', {'movies': movies})
