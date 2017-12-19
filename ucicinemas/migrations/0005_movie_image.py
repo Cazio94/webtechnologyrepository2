da82e11943f975af2b9336858ee9b08c721a789a
cazio94 <a.catani22@gmail.com>
2017-12-19 18:17:29 +0100

    added field image on models

---
+++ ucicinemas/migrations/0005_movie_image.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ucicinemas', '0004_auto_20171219_1806'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='movie',
+            name='image',
+            field=models.URLField(default=1),
+            preserve_default=False,
+        ),
+    ]
